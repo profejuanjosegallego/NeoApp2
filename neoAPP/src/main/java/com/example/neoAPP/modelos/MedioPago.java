12907fd0510c59a224400385a5fa23843d01e546
Julio Carrillo <carrillojulio470@gmail.com>
2026-02-23 15:49:25 -0500

    feat se anexan atributos y se crean algunas anotaciones en el modelo de usuario

--- neoAPP/src/main/java/com/example/neoAPP/modelos/MedioPago.java
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/MedioPago.java
@@ -3,5 +3,54 @@ package com.example.neoAPP.modelos;
 public class MedioPago {
 
     //id,nombre,franquicia,estado(activo/inactivo)
+    private int id;
+    private String nombre;
+    private String franquicia;
+    private String estado;
+
+    public MedioPago() {
+    }
+
+    public MedioPago(int id, String nombre, String franquicia, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.franquicia = franquicia;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getFranquicia() {
+        return franquicia;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFranquicia(String franquicia) {
+        this.franquicia = franquicia;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+    
 
 }
--- neoAPP/src/main/java/com/example/neoAPP/modelos/Usuario.java
+++ neoAPP/src/main/java/com/example/neoAPP/modelos/Usuario.java
@@ -1,7 +1,76 @@
 package com.example.neoAPP.modelos;
 
-public class Usuario {
+import org.hibernate.annotations.IdGeneratorType;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     //id,nombres,tipodocumento,documento,edad
+    private int id;
+    private String nombres;
+    private String tipodocumento;
+    private String documento;
+    private int edad;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombres, String tipodocumento, String documento, int edad) {
+        this.id = id;
+        this.nombres = nombres;
+        this.tipodocumento = tipodocumento;
+        this.documento = documento;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getTipodocumento() {
+        return tipodocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setTipodocumento(String tipodocumento) {
+        this.tipodocumento = tipodocumento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
 
 }
