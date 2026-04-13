e22abbf1eadb708b35e87be4875dc5a6b3cab929
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-04-13 10:16:22 -0500

    se crea sertvicio guardar y listar

--- neoAPP/src/main/java/com/example/neoAPP/servicios/UsuarioServicio.java
+++ neoAPP/src/main/java/com/example/neoAPP/servicios/UsuarioServicio.java
@@ -1,8 +1,13 @@
 package com.example.neoAPP.servicios;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.neoAPP.modelos.Usuario;
 import com.example.neoAPP.repositorios.IUsuarioRepositorio;
 
 @Service
@@ -16,8 +21,39 @@ public class UsuarioServicio {
 
 
     //funcion para guardar un usuario
+    public Usuario guardar_usuario(Usuario datosUsuario){
+        //validar los campos del modelo segun la LN
+
+        //validar que el usuario me mande sus nombres
+        if(datosUsuario.getNombres()==null || datosUsuario.getNombres().isEmpty() || datosUsuario.getNombres().isBlank()){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Apreciado usuario, el nombre de usuario es obligatorio"
+            );
+
+        }
+
+        //valdiar documento tenga al menos 6 caracteres
+        if(datosUsuario.getDocumento().length()<6){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Apreciado usuario, el documento debe tener mas de 6 caracteres"
+            );
+
+        }
+
+        //Si paso todas las validaciones
+        //intentare activar el guardado de los datos
+        return repositorio.save(datosUsuario);
+
+    }
 
     //funcion para listar todos los usuarios
+
+    public List<Usuario> listar_usuarios(){
+        return repositorio.findAll();
+    }
+
 
     //funcion para modificar un usuario
 
