b5f34434df762fc4fc8e94da0e4a3336fc213bd8
maria.mejia <desarrollo@academicos.cesde.edu.co>
2026-05-04 11:12:08 -0500

    ok

--- neoAPP/src/main/java/com/example/neoAPP/controladores/UsuarioControaldor.java
+++ neoAPP/src/main/java/com/example/neoAPP/controladores/UsuarioControaldor.java
@@ -3,8 +3,11 @@ package com.example.neoAPP.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +31,24 @@ public class UsuarioControaldor {
     public ResponseEntity<?> controaldorListar(){
         return ResponseEntity.status(HttpStatus.OK).body(servicio.listar_usuarios());
     }
+
+    //control para modificar
+    @PutMapping("/{id}")
+     public ResponseEntity<?> controaldorModificar(@PathVariable Integer id, @RequestBody Usuario datos){
+        return ResponseEntity.status(HttpStatus.OK).body(servicio.modificar_usuario(id,datos));
+    }
+
+    //control para eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> controaldorEliminar(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(servicio.eliminar_usuario(id));
+    }
+
+    //control para buscar por id
+    @GetMapping("/{id}")
+     public ResponseEntity<?> controaldorBuscarPorId(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(servicio.buscar_usuario_por_id(id));
+    }
+
 
 }
